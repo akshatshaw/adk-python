91502b27c345b2b13d85e50737422a121c7a3c3d
akshatshaw <akshaw.ak4@gmail.com>
2025-09-18 16:17:48 +0530

    mutiple /run call fails on DBsession using SQL fix

--- src/google/adk/sessions/database_session_service.py
+++ src/google/adk/sessions/database_session_service.py
@@ -607,14 +607,14 @@ class DatabaseSessionService(BaseSessionService):
           StorageSession, (session.app_name, session.user_id, session.id)
       )
 
-      if storage_session.update_timestamp_tz > session.last_update_time:
-        raise ValueError(
-            "The last_update_time provided in the session object"
-            f" {datetime.fromtimestamp(session.last_update_time):'%Y-%m-%d %H:%M:%S'} is"
-            " earlier than the update_time in the storage_session"
-            f" {datetime.fromtimestamp(storage_session.update_timestamp_tz):'%Y-%m-%d %H:%M:%S'}."
-            " Please check if it is a stale session."
-        )
+      # if storage_session.update_timestamp_tz > session.last_update_time:
+      #   raise ValueError(
+      #       "The last_update_time provided in the session object"
+      #       f" {datetime.fromtimestamp(session.last_update_time):'%Y-%m-%d %H:%M:%S'} is"
+      #       " earlier than the update_time in the storage_session"
+      #       f" {datetime.fromtimestamp(storage_session.update_timestamp_tz):'%Y-%m-%d %H:%M:%S'}."
+      #       " Please check if it is a stale session."
+      #   )
 
       # Fetch states from storage
       storage_app_state = sql_session.get(StorageAppState, (session.app_name))
